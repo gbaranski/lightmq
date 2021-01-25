cd883260dc942dafc55269cb9515046dd0a4ade6
gbaranski <gbaranski19@gmail.com>
2021-01-25 12:34:17 +0100

    test: add testing store

--- store_test.go
+++ store_test.go
@@ -1,10 +1,87 @@
 package lightmq
 
-func TestClientStore() {
-	cs := NewClientStore()
-	cs.Add(Client{
-		ClientID:  "someClientID",
-		IPAddress: nil,
-	})
+import "testing"
 
+func TestClientStore(t *testing.T) {
+	cs := NewClientStore()
+	c1 := Client{
+		ClientID:  "SomeClientID",
+		IPAddress: nil,
+	}
+	c2 := Client{
+		ClientID:  "SomeDifferentClientID",
+		IPAddress: nil,
+	}
+
+	if err := cs.Add(c1); err != nil {
+		t.Fatalf("fail add c1 %s", err.Error())
+	}
+	if err := cs.Add(c2); err != nil {
+		t.Fatalf("fail add c2 %s", err.Error())
+	}
+
+	c, ok := cs.Get(c1.ClientID)
+	if !ok {
+		t.Fatalf("fail retreive c1")
+	}
+	if c.ClientID != c1.ClientID {
+		t.Fatalf("invalid c1 clientID")
+	}
+
+	c, ok = cs.Get(c2.ClientID)
+	if !ok {
+		t.Fatalf("fail retreive c2")
+	}
+	if c.ClientID != c2.ClientID {
+		t.Fatalf("invalid c2 clientID")
+	}
+}
+
+func TestClientStoreNotPresentUser(t *testing.T) {
+	cs := NewClientStore()
+	c1 := Client{
+		ClientID:  "SomeClientID",
+		IPAddress: nil,
+	}
+
+	c2 := Client{
+		ClientID:  "SomeDifferentClientID",
+		IPAddress: nil,
+	}
+
+	if err := cs.Add(c1); err != nil {
+		t.Fatalf("fail add c1 %s", err.Error())
+	}
+
+	c, ok := cs.Get(c1.ClientID)
+	if !ok {
+		t.Fatalf("fail retreive c1")
+	}
+	if c.ClientID != c1.ClientID {
+		t.Fatalf("invalid c1 clientID")
+	}
+
+	c, ok = cs.Get(c2.ClientID)
+	if ok {
+		t.Fatalf("expected to fail on c2")
+	}
+	if c.ClientID == c2.ClientID {
+		t.Fatalf("invalid c2 clientID")
+	}
+}
+
+func TestClientStoreDuplicate(t *testing.T) {
+	cs := NewClientStore()
+	c1 := Client{
+		ClientID:  "SomeClientID",
+		IPAddress: nil,
+	}
+
+	if err := cs.Add(c1); err != nil {
+		t.Fatalf("fail add c1 %s", err.Error())
+	}
+
+	if err := cs.Add(c1); err == nil {
+		t.Fatalf("expected error, received nil while adding dup")
+	}
 }
